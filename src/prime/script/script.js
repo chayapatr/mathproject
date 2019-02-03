c64c4ab79f7c08a445b780b1dc5bb5277005d34a
chayapatr <chayapatr.arc@gmail.com>
2019-02-03 12:57:26 +0700

    finish prime gen + add manifest

--- src/prime/script/script.js
+++ src/prime/script/script.js
@@ -1,19 +1,27 @@
 import footerDate from '../../script/ui.js'
 import nav from '../../script/subpage'
 import sieve from 'sieve-of-eratosthenes'
+import pullAll from 'lodash.pullall'
+import takeRightWhile from 'lodash.takerightwhile'
+
 (footerDate)();
 (nav)()
 
 document.getElementById('button').addEventListener('click', () => {
-    let from = document.getElementById('from').value || 3
-    let to = document.getElementById('to').value || 3
+    let from = document.getElementById('from').value || 2
+    let to = document.getElementById('to').value || 100
+    let toArr = sieve(++to)
+    let fromArr = sieve(from)
+    let dom = ``
     document.getElementById('from').value = document.getElementById('from').value || 2
-    document.getElementById('to').value = document.getElementById('to').value || 3
-    let array = sieve(++to)
-    let dom = ''
-    array.forEach(el => {
+    document.getElementById('to').value = document.getElementById('to').value || 100
+    let resArr = pullAll(toArr,fromArr)
+    dom = dom + `result (${resArr.length}) : `
+    resArr.forEach(el => {
         dom = dom + `${el} `
     })
     document.getElementById('result').innerText = dom
     document.getElementById('reshero').style.display = 'block'
+    /*document.getElementById('input').style.display = 'none'
+    document.getElementById('hero-under-title').style.display = 'none'*/
 })
