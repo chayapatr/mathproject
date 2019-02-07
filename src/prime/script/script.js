a79c403bd2aba23177c0cd43773908150203dc44
chayapatr <chayapatr.arc@gmail.com>
2019-02-07 23:05:54 +0700

    fix prime js style + change default

--- src/prime/script/script.js
+++ src/prime/script/script.js
@@ -1,27 +1,25 @@
-import footerDate from '../../script/ui.js'
-import nav from '../../script/subpage'
-import sieve from 'sieve-of-eratosthenes'
-import pullAll from 'lodash.pullall'
+import footerDate from '../../script/ui.js';
+import nav from '../../script/subpage';
+import sieve from 'sieve-of-eratosthenes';
+import pullAll from 'lodash.pullall';
 
-(footerDate)();
-(nav)()
+footerDate();
+nav();
 
 document.getElementById('button').addEventListener('click', () => {
-    let from = document.getElementById('from').value || 1
-    let to = document.getElementById('to').value || 99
-    let toArr = sieve(++to)
-    let fromArr = sieve(from)
-    let dom = ``
-    document.getElementById('from').value = from
-    document.getElementById('to').value = to
-    let resArr = pullAll(toArr,fromArr)
-    dom = dom + `result (${resArr.length}) : `
-    resArr.forEach(el => {
-        dom = dom + `${el} `
-    })
+  let from = document.getElementById('from').value || 1;
+  let to = document.getElementById('to').value || 9999;
+  let toArr = sieve(++to);
+  let fromArr = sieve(from);
+  let dom = ``;
+  document.getElementById('from').value = from;
+  document.getElementById('to').value = to;
+  let resArr = pullAll(toArr, fromArr);
+  dom = dom + `result (${resArr.length}) : `;
+  resArr.forEach(el => {
+    dom = dom + `${el} `;
+  });
 
-    document.getElementById('result').innerText = dom
-    document.getElementById('reshero').style.display = 'block'
-    /*document.getElementById('input').style.display = 'none'
-    document.getElementById('hero-under-title').style.display = 'none'*/
-})
+  document.getElementById('result').innerText = dom;
+  document.getElementById('reshero').style.display = 'block';
+});
